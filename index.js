6ecc87e5177b1f29a387c80415a64464279cfe8e
Adam Sommer <adam@thehoick.com>
2016-07-01 06:41:55 -0400

    Moved all database methods into DataApi and setup a GET and POST route for creating and displaying Audios via JSON.

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ var Datastore = require('nedb');
 var bodyParser = require('body-parser')
 var os = require('os');
 
-var PilaApi = require('./lib/pila_api');
-
-var db = new Datastore({ filename: 'db/audiopila.db', autoload: true });
-db.ensureIndex({ fieldName: 'name' }, function (err) {
-  if (err) {
-    console.log('error creating db index name err:', err);
-  }
-});
-
+var DataApi = require('./lib/data_api');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// GET / (index of devices)
+// GET / (index of everything)
 app.get('/', function(req, res) {
-  db.find({}, function (err, docs) {
-    console.log('docs:', docs);
-    res.json({pilas: docs});
-  });
+  DataApi.findAll((docs) => {
+    res.json(docs);
+  })
+});
+
+// GET / (index of audios)
+app.get('/audios', function(req, res) {
+  DataApi.getAudios((audios) => {
+    res.json(audios);
+  })
 });
 
 // POST /audios (add Audios in directory)
 app.post('/audios', function(req, res) {
   console.log('req.body:', req.body);
-    PilaApi.getLocalFiles(req.body.path, (files) => {
+    DataApi.getLocalFiles(req.body.path, (files) => {
       var parts = req.body.path.split('/');
       var repository = {name: parts[parts.length - 1], path: req.body.path};
 
@@ -39,14 +37,19 @@ app.post('/audios', function(req, res) {
         }
       })
 
-      PilaApi.getAudios(audioFiles, req.body.path, repository, (audios) => {
-        db.insert({audios: audios}, function(error, doc) {
-          if (error) {
-            console.log('audios db.insert error:', error);
+      // Use a counter to execute the res.json.
+      var audios = {};
+      var counter = 0;
+
+      audioFiles.forEach((file) => {
+        DataApi.getAudio(file, req.body.path, repository, (audio) => {
+          audios[audio.slug] = audio;
+          counter++;
+          if (counter == audioFiles.length) {
+            res.json({message: 'Successfully added audios.', audios: audios});
           }
-          res.json({message: 'Successfully added audios.', audios: audios});
-        })
-      });
+        });
+      })
     });
 })
 
@@ -54,44 +57,26 @@ app.post('/audios', function(req, res) {
 app.post('/sync', function(req, res) {
   console.log('syncing:', req.body.name);
 
-  var pila = {
+  var me = {
     name: os.hostname(),
     platform: os.platform(),
     audios: [],
     lastPlayed: undefined,
     lastSynced: Date.now(),
     syncedFrom: req.body.name,
-    httpUrl: req.protocol + '://' + req.get('host') + req.originalUrl
+    httpUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
+    type: 'pila'
   }
 
-  // Find the device by name.
-  db.find({name: req.body.name}, function(error, docs) {
-    if (docs.length == 0) {
-      db.insert(req.body, function (err, doc) {
-        db.find({}, function (err, docs) {
-          res.json({message: 'Sync successful.', pilas: docs});
-        });
-      });
+  DataApi.getPila(req.body.name, (pilas) => {
+    if (pilas == null) {
+      DataApi.addPila(req.body, (pilas) => {
+        res.json({message: 'Sync successful.', pilas: docs});
+      })
     } else {
-      // console.log('docs:', docs);
-      db.update({ name: req.body.name }, req.body, {}, function (err, numReplaced) {
-        if (err) {
-          console.log('update err;', err);
-        }
-
-        db.find({}, function (err, docs) {
-          // Add this app to the list.
-          docs[docs.length] = pila;
-
-          // Create an object from the docs Array.
-          var pilas = {};
-          docs.forEach((doc) => {
-            pilas[doc.name] = doc;
-          })
-
-          res.json({message: 'Sync successful.', pilas: pilas, pila: pila});
-        });
-      });
+      DataApi.updatePila(req.body, me, (pilas) => {
+        res.json({message: 'Sync successful.', pilas: pilas, pila: pilas[req.body.name]});
+      })
     }
   })
 });
